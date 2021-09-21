a29902ac4d6ded1b1b341018769543f0f301390b
Emmanuel Nol <42006907+MalazanIcarium@users.noreply.github.com>
2021-09-21 17:42:03 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 let holaVeces = parseInt (prompt('Introduzca un numero entre 1 y 100'));
 let acumulador = ``;
-
+    /* Incluye un IF para asegurarnos que los parametros sean los requeridos. */
 if( holaVeces > 0 && holaVeces < 101 ){
       for (let i = 1 ; i <= holaVeces ; i++) {
        acumulador += `<p> Hola! Pasaste por aca ${i} veces! </p>`
     }
 } else {
     alert('Por favor introduzca un numero valido.');
+    /* Añadimos un Reload para poder pasar un parametro valido desde el prompt inicial. */
     location.reload();
     }
 
